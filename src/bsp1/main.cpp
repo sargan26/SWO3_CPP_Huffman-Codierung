cef5357ea4b429e03e11e896490bdf9a28388464
Daniel Mitterlehner <daniel.mitterlehner404@gmail.com>
2024-01-02 15:32:20 +0100

    Test

--- src/bsp1/main.cpp
+++ src/bsp1/main.cpp
@@ -1,14 +1,15 @@
 #include "frequency_table.h"
 #include <iostream>
+#include <filesystem>
 
 int main () {
 	// Datei öffnen, Frequenzen berechnen und ausgeben
 	FrequencyTable table;
-	table.readFile("Text2.txt");
+	table.readFile("Text3.txt");
 	table.calculateFrequencies();
 	table.printTable();
 
 
-
+	std::cout << std::filesystem::current_path().string() << std::endl;
 
 }
